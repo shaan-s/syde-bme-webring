aaa68ecf04e00fa7e652280ace79a7e4c06f178b
shaan-s <shaan.sawrup@gmail.com>
2025-03-23 13:22:27 -0400

    update with content

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function createComponents() {
-    console.log(siteData.length)
-}
+function createComponents(sites) {
+    const sitebox = document.getElementById("site-box");
+    sites.forEach((person, index) => {
+        const cnt1 = document.createElement("div");
+        cnt1.className = "cell";
+        cnt1.textContent = person.name;
+        const cnt2 = document.createElement("div");
+        cnt2.className = "cell";
+        cnt2.textContent = person.cohort;
+        const cnt3 = document.createElement("div");
+        const link = document.createElement("a");
+        cnt3.className = "cell";
+        link.textContent = person.website;
+        link.href = person.website;
+        cnt3.appendChild(link);
 
+        sitebox.appendChild(cnt1);
+        sitebox.appendChild(cnt2);
+        sitebox.appendChild(cnt3);
+        
+    });
+}
 
 createComponents(siteData.sites);
