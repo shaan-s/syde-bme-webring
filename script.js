87403abdd945e83589fa71410e7a0ec8c8ea5bb6
shaan-s <shaan.sawrup@gmail.com>
2025-03-23 14:57:04 -0400

    filter by program

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function createComponents(sites) {
+function createComponents(sites,selected) {
+    console.log("t");
+    var curr_class;
     const sitebox = document.getElementById("site-box");
+    sitebox.innerHTML = "";
     sites.forEach((person, index) => {
+        if (person.program == selected || selected == "") {
+            curr_class = "cell";
+        }
+        else {curr_class = "cell unlight";}
+        
         const cnt1 = document.createElement("div");
-        cnt1.className = "cell";
+        cnt1.className = curr_class;
         cnt1.textContent = person.name;
         const cnt2 = document.createElement("div");
-        cnt2.className = "cell";
-        cnt2.textContent = person.cohort;
+        cnt2.className = curr_class;
+        cnt2.textContent = person.year;
         const cnt3 = document.createElement("div");
         const link = document.createElement("a");
-        cnt3.className = "cell";
+        cnt3.className = curr_class;
         link.textContent = person.website;
         link.href = person.website;
         cnt3.appendChild(link);
@@ -21,4 +29,16 @@ function createComponents(sites) {
     });
 }
 
-createComponents(siteData.sites);
+createComponents(siteData.sites,"")
+
+document.getElementById("BME").addEventListener("click", function() {
+    createComponents(siteData.sites,"BME")
+  });
+
+document.getElementById("SYDE").addEventListener("click", function() {
+    createComponents(siteData.sites,"SYDE")
+});
+
+document.getElementById("SYDE").addEventListener("click", function() {
+    createComponents(siteData.sites,"SYDE")
+});
