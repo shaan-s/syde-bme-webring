39da187dbe6692a0c59ba5c929d4e20eae253a93
shaan-s <shaan.sawrup@gmail.com>
2025-03-23 16:17:13 -0400

    css

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ function createComponents(sites,selected,search) {
     var curr_class;
     const sitebox = document.getElementById("site-box");
     sitebox.innerHTML = "";
+    
+    const head1 = document.createElement("div");
+    head1.textContent = "NAME";
+    const head2 = document.createElement("div");
+    head2.textContent = "YEAR";
+    const head3 = document.createElement("div");
+    head3.textContent = "WEBSITE";
+    head1.className = "head-cell cell";
+    head2.className = "head-cell cell";
+    head3.className = "head-cell cell";
+    sitebox.appendChild(head1);
+    sitebox.appendChild(head2);
+    sitebox.appendChild(head3);
+
     sites.forEach((person, index) => {
         curr_class = "cell unlight";
         let search_l = search.toLowerCase();
@@ -18,7 +32,7 @@ function createComponents(sites,selected,search) {
             if ((person.name.toLowerCase()).includes(search_l) || (person.year).includes(search_l)) {
                 curr_class = "cell";
         }}
-        
+
         const cnt1 = document.createElement("div");
         cnt1.className = curr_class;
         cnt1.textContent = person.name;
