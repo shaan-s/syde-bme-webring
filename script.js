8776a4b201fa1c26288509a4a705fd6175b68ec9
shaan-s <shaan.sawrup@gmail.com>
2025-03-24 03:14:01 -0400

    add favicon, bg, css changes

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+var selected = "";
+
 function formatUrl(url) {
     return url.replace(/(^\w+:|^)\/\//, '').replace(/^www\./, "");
 }
 
 function createComponents(sites,selected,search) {
+    
     var curr_class;
     const sitebox = document.getElementById("site-box");
     sitebox.innerHTML = "";
@@ -20,17 +23,20 @@ function createComponents(sites,selected,search) {
     sitebox.appendChild(head2);
     sitebox.appendChild(head3);
 
+    var highlighted = 0;
     sites.forEach((person, index) => {
         curr_class = "cell unlight";
         let search_l = search.toLowerCase();
 
         if (person.program == selected) {
             curr_class = "cell";
+            highlighted++;
         }
         
         if (selected == "") {
             if ((person.name.toLowerCase()).includes(search_l) || (person.year).includes(search_l)) {
                 curr_class = "cell";
+                highlighted++;
         }}
 
         const cnt1 = document.createElement("div");
@@ -49,18 +55,41 @@ function createComponents(sites,selected,search) {
         sitebox.appendChild(cnt1);
         sitebox.appendChild(cnt2);
         sitebox.appendChild(cnt3);
+
+        if (highlighted != siteData.sites.length) {
+            document.getElementById("highlighted").innerHTML = ", " + highlighted + " highlighted";
+        }
+        else {document.getElementById("highlighted").innerHTML = "";}
+        
         
     });
 }
 
+function updateBtns(curr_state) {
+    document.getElementById("BME").classList.remove("highlight");
+    document.getElementById("SYDE").classList.remove("highlight");
+    if (curr_state == selected) {
+        selected = "";
+    }
+    else {selected = curr_state;}
+    if (selected == "BME") {
+        document.getElementById("BME").classList.add("highlight");
+    }
+    else if (selected == "SYDE") {
+        document.getElementById("SYDE").classList.add("highlight");
+    }
+    createComponents(siteData.sites,selected,"");
+}
+
+document.getElementById("sitescount").innerHTML = siteData.sites.length;
 createComponents(siteData.sites,"","")
 
-document.getElementById("BME").addEventListener("click", function() {
-    createComponents(siteData.sites,"BME","")
+document.getElementById("BME").addEventListener("click", function() { 
+    updateBtns("BME");
   });
 
 document.getElementById("SYDE").addEventListener("click", function() {
-    createComponents(siteData.sites,"SYDE","")
+    updateBtns("SYDE");
 });
 
 document.getElementById("searchbar").addEventListener("keyup", function() {
